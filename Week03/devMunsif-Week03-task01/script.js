3b533f801a85b3260ef3c06efb37910248995974
MunsifKhan1 <munsifk597@gmail.com>
2024-12-25 11:04:43 +0500

    add a new changes in devMunsif-week03-task01

--- Week03/devMunsif-Week03-task01/script.js
+++ Week03/devMunsif-Week03-task01/script.js
@@ -1,10 +1,61 @@
-let age= prompt("enter your age");
-age=Number(age);
-if(age<18){
-    console.log("you are not eligible for licence ")
+// let age= prompt("enter your age");
+// if(age<18){
+//     console.log("you are not eligible for licence ")
 
-}else if(age>=18 && age<60){
-    console.log("you are eligible for licence")
+// }else if(age>=18 && age<60){
+//     console.log("you are eligible for licence")
+// }else{
+//     console.log("You are a senior citizen and eligible for licence.")
+// }
+
+
+
+
+// let day=prompt("Enter a number (1-7)for the days of week");
+// if(day==1){
+//     alert("Monday")
+// }else if(day==2){
+//     console.log("Tuseday")
+// }else if(day==3){
+//     console.log("Wednesday")
+// }else if(day==4){
+//     console.log("Thursday")
+// }else if(day==5){
+//     console.log("Friday")
+// }else if(day==6){
+//     console.log("Saturday")
+// }else if(day==7){
+//     console.log("Sunday")
+// }else{
+// console.log("invalid day! please enter the number between 1and 7.")
+// }
+
+
+
+// let number=4;
+// if(number%2===0){
+//     console.log("the number is even.")
+// }else if(number%2===1){
+//     console.log("the number is odd.")
+// } else{
+//     console.log("you enter the number is invelid")
+// }
+
+
+
+
+
+let marks=prompt("Please enter your marks")
+if(marks>=90){
+    alert("Your grade is : A+")
+}else if(marks>=80){
+    alert("Your grade is : A")
+}else if(marks>=70){
+    alert("Your grade is : B")
+}else if(marks>=60){
+    alert("Your grade is : C")
+}else if(marks>=50 && marks<=40){
+    alert("Your grade is : D")
 }else{
-    console.log("You are a senior citizen and eligible to vote.")
+    alert("You are fail")
 }
