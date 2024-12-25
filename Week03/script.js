47b537f8d05eb6ad75221ab8e8158142370225f8
USAMA KHAN <usamaisrar45@gmail.com>
2024-12-25 01:52:39 -0800

    addede

--- Week03/script.js
+++ Week03/script.js
@@ -1,14 +1,27 @@
-let age= prompt("Enter your age : ")
-age=Number(age);
-
-if(age<20)
-    {
-    console.log("you are not eligible for licence ")
-}
-else if(age>=20 && age<60)
-    {
-    console.log("you are eligible for licence")
-}
-else{
-    console.log("You are a senior citizen and eligible to licence.")
+let day=3;
+switch(day)
+{
+    case 1:
+    console.log("Monday");
+    break;
+    case 2:
+    console.log("Tuesday");
+    break;
+    case 3:
+    console.log("wednesday");
+    break;
+    case 4:
+    console.log("Thrsday");
+    break;
+    case 5:
+    console.log("friday");
+    break;
+    case 6:
+    console.log("saturday");
+    break;
+    case 7:
+    console.log("sunday");
+    break;
+    default:
+        console.log("invelide")
 }
