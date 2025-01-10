30aa56444fc2d84ee265fcfc82eba14c819150c6
devWaheed <wakkhan947@gmail.com>
2025-01-10 17:33:46 +0500

    Done changes

---
+++ Week06/firstApp/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+
+  return (
+    <div>
+      <p className = "navbar">This is Navbar
+      </p>
+    </div>
+  )
+}
+
+export default Navbar
