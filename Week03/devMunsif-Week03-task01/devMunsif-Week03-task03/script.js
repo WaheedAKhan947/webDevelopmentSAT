5485f223f24758a28f1954b455940478defc4469
MunsifKhan1 <munsifk597@gmail.com>
2025-01-11 14:53:30 +0500

    add a code of devMunsif

--- Week03/devMunsif-Week03-task01/devMunsif-Week03-task03/script.js
+++ Week03/devMunsif-Week03-task01/devMunsif-Week03-task03/script.js
@@ -1,7 +1,14 @@
-let student={
-    name:"munsif",
-    age:23,
-    address:"swabi",
-}
-Object.assign(student,{ gander:"male"})
-console.log(student)
+// let student={
+//     name:"munsif",
+//     age:23,
+//     address:"swabi",
+// }
+// Object.assign(student,{ phone:311})
+// console.log(student)
+// let human={
+//     name:"khan",
+//     age:33,
+//     address:"swabi",
+// }
+// Object.assign(human,{gander:24});
+// console.log(human);
