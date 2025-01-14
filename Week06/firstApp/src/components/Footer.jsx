6972c692e8eb7e04c7087a1b141a5f4fbcfa674a
USAMA KHAN <usamaisrar45@gmail.com>
2025-01-14 04:21:28 -0800

    react

--- Week06/firstApp/src/components/Footer.jsx
+++ Week06/firstApp/src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import './footer.css'
+
 const Footer = () => {
+
   return (
     <div>
-      <p>This is footer component</p>
+      <p className = "footer">This is footer
+      </p>
     </div>
   )
 }
-
 export default Footer
---
+++ Week06/firstApp/src/components/Form/Form.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./../Form.css";
+const Form = () => {
+  return (
+ 
+      <div className='form'>
+        <div className="tital"><h1>Form in React</h1></div>
+        <div className="form-input">
+        
+            <input type="text" placeholder='User name' />
+            <input type="Email" placeholder='User Email' />
+            <input type="text" placeholder='User Phone number' />
+            <input type="text" placeholder='User Address' />
+            <input type="password" placeholder='User Password' />
+            <input type="password" placeholder='User Confirm Password ' />
+        </div>
+        <button onClick={()=>confirm( "You clicked the button!")} >submit</button>
+    </div>
+  );
+}
+
+export default Form;
